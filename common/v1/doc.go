336948977ae13dd260f6ad591c68d7d77489d39c
Ewout Prangsma <ewout@prangsma.net>
2019-01-17 16:20:04 +0100

    Ensure common package gets compiled

---
+++ common/v1/doc.go
@@ -0,0 +1,12 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+// Package common/v1 contains the common types used in API of ArangoDB Managed Service
+package v1
+
+//go:generate protoc -I .:../../:../../vendor/ --gofast_out=Mgithub.com/golang/protobuf/ptypes/timestamp/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:. ./common.proto
