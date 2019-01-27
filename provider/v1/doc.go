0538b407b524eff45b3cffaaa9304fbbc3dde712
Ewout Prangsma <ewout@prangsma.net>
2019-01-27 19:05:48 +0100

    Added provider API

---
+++ provider/v1/doc.go
@@ -0,0 +1,12 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+// Package provider/v1 contains the API used to fetch provider & region information.
+package v1
+
+//go:generate protoc -I .:../../:../../vendor/:../../vendor/googleapis/:../../vendor/github.com/gogo/protobuf/protobuf/ --gofast_out=Mgithub.com/golang/protobuf/ptypes/timestamp/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:. --grpc-gateway_out=paths=source_relative,logtostderr=true,allow_delete_body=true:. ./provider.proto
