60677211eaf7849ac5d153af22db2b8342bc0209
Ewout Prangsma <ewout@prangsma.net>
2019-02-15 17:13:27 +0100

    Added EventTypeCACertificateUpdated

--- crypto/v1/eventtypes.go
+++ crypto/v1/eventtypes.go
@@ -14,6 +14,9 @@ const (
 	// EventTypeCACertificateCreated is the type of event fired after a CA certificate has been created
 	// SubjectID contains the ID of the CA certificate.
 	EventTypeCACertificateCreated = "crypto.cacertificate.created"
+	// EventTypeCACertificateUpdated is the type of event fired after a CA certificate has been updated
+	// SubjectID contains the ID of the CA certificate.
+	EventTypeCACertificateUpdated = "crypto.cacertificate.updated"
 	// EventTypeCACertificateDeleted is the type of event fired after a CA certificate has been (marked for) deleted
 	// SubjectID contains the ID of the CA certificate.
 	EventTypeCACertificateDeleted = "crypto.cacertificate.deleted"
