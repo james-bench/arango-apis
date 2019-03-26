e6168134db4727f76fe74618a9659fb8b9222a65
Ewout Prangsma <ewout@prangsma.net>
2019-03-26 09:03:43 +0100

    Fixed usage test

--- usage/v1/urls_test.go
+++ usage/v1/urls_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 func TestUsageURL(t *testing.T) {
-	assert.Equal(t, "/Organization/123/Usage/u1", UsageURL("123", "u1"))
-	assert.Equal(t, "/Organization/123%2F567/Usage/c2%2F3", UsageURL("123/567", "c2/3"))
-	assert.Equal(t, "/Organization/123%2F567/Usage/a%25b", UsageURL("123/567", "a%b"))
+	assert.Equal(t, "/Organization/123/UsageItem/u1", UsageItemURL("123", "u1"))
+	assert.Equal(t, "/Organization/123%2F567/UsageItem/c2%2F3", UsageItemURL("123/567", "c2/3"))
+	assert.Equal(t, "/Organization/123%2F567/UsageItem/a%25b", UsageItemURL("123/567", "a%b"))
 }
