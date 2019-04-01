89437269d603ca8fc3213aca6dd56ef63109d63f
Ewout Prangsma <ewout@prangsma.net>
2019-04-01 15:37:50 +0200

    Added GetOrCreate helper on Deployment

--- data/v1/helpers.go
+++ data/v1/helpers.go
@@ -19,3 +19,35 @@ func DeploymentStatusEqual(a, b *Deployment_Status) bool {
 		a.GetUpgrading() == b.GetUpgrading() &&
 		strings.Join(a.GetServerVersions(), ",") == strings.Join(b.GetServerVersions(), ",")
 }
+
+// GetOrCreateCertificates returns the Certificates field, creating it if needed.
+func (d *Deployment) GetOrCreateCertificates() *Deployment_CertificateSpec {
+	if d.GetCertificates() == nil {
+		d.Certificates = &Deployment_CertificateSpec{}
+	}
+	return d.GetCertificates()
+}
+
+// GetOrCreateExpiration returns the Expiration field, creating it if needed.
+func (d *Deployment) GetOrCreateExpiration() *Deployment_Expiration {
+	if d.GetExpiration() == nil {
+		d.Expiration = &Deployment_Expiration{}
+	}
+	return d.GetExpiration()
+}
+
+// GetOrCreateServers returns the Servers field, creating it if needed.
+func (d *Deployment) GetOrCreateServers() *Deployment_ServersSpec {
+	if d.GetServers() == nil {
+		d.Servers = &Deployment_ServersSpec{}
+	}
+	return d.GetServers()
+}
+
+// GetOrCreateStatus returns the Status field, creating it if needed.
+func (d *Deployment) GetOrCreateStatus() *Deployment_Status {
+	if d.GetStatus() == nil {
+		d.Status = &Deployment_Status{}
+	}
+	return d.GetStatus()
+}
