43bc25cfd20cbd16145d6e400bfacec4aa37c853
Ewout Prangsma <ewout@prangsma.net>
2019-06-16 11:33:18 +0200

    Added more quota kinds

--- data/v1/quotakinds.go
+++ data/v1/quotakinds.go
@@ -14,6 +14,24 @@ const (
 	// QuotaKindDeploymentsPerProject limits the number of deployments in a single project.
 	// This kinds of quota must be requested on project level.
 	QuotaKindDeploymentsPerProject = "data.deployments-per-project"
+	// QuotaKindDeploymentCoordinatorMemory limits the amount of memory (in GB) used
+	// by a single coordinator in a deployment.
+	// This kinds of quota must be requested on project level.
+	QuotaKindDeploymentCoordinatorMemory = "data.deployment-coordinator-memory"
+	// QuotaKindDeploymentCoordinators limits the number of coordinators per deployment.
+	// This kinds of quota must be requested on project level.
+	QuotaKindDeploymentCoordinators = "data.deployment-coordinators"
+	// QuotaKindDeploymentDBServerMemory limits the amount of memory (in GB) used
+	// by a single dbserver in a deployment.
+	// This kinds of quota must be requested on project level.
+	QuotaKindDeploymentDBServerMemory = "data.deployment-dbserver-memory"
+	// QuotaKindDeploymentDBServerDisk limits the amount of disk (in GB) used
+	// by a single dbserver in a deployment.
+	// This kinds of quota must be requested on project level.
+	QuotaKindDeploymentDBServerDisk = "data.deployment-dbserver-disk"
+	// QuotaKindDeploymentDBServers limits the number of dbservers per deployment.
+	// This kinds of quota must be requested on project level.
+	QuotaKindDeploymentDBServers = "data.deployment-dbservers"
 	// QuotaKindTotalDeploymentsMemoryPerProject limits the total amount of memory used
 	// by all deployments in a project in GB.
 	// This kinds of quota must be requested on project level.
