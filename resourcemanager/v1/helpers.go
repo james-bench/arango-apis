e3b9910b6d02f6c130299db54ddd90497b1639e2
Ewout Prangsma <ewout@prangsma.net>
2019-06-17 09:23:13 +0200

    Added quote-list helpers

---
+++ resourcemanager/v1/helpers.go
@@ -0,0 +1,30 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package v1
+
+// Contains returns true if the given quota list contains a quota of given kind.
+func (list *QuotaList) Contains(kind string) bool {
+	for _, q := range list.GetItems() {
+		if q.GetKind() == kind {
+			return true
+		}
+	}
+	return false
+}
+
+// Get returns the first quota with given kind from the given list or nil if no
+// such quota exists.
+func (list *QuotaList) Get(kind string) *Quota {
+	for _, q := range list.GetItems() {
+		if q.GetKind() == kind {
+			return q
+		}
+	}
+	return nil
+}
