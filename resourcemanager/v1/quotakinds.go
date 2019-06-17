134aa1d47c2afcef63d69844ff510c23f63d0c73
Ewout Prangsma <ewout@prangsma.net>
2019-06-17 09:07:55 +0200

    Added total deployment/tier quota kind template

--- resourcemanager/v1/quotakinds.go
+++ resourcemanager/v1/quotakinds.go
@@ -14,4 +14,10 @@ const (
 	// QuotaKindProjectsPerOrganization limits the number of projects in a single organization.
 	// This kinds of quota must be requested on organization level.
 	QuotaKindProjectsPerOrganization = "resourcemanager.projects-per-organization"
+	// QuotaKindTotalDeploymentsPerTierTemplate is a go-template for a quota kind
+	// tha limits the total number of deployments (over the lifetime on the organization)
+	// in a specific tier.
+	// The template variable is the tier-id.
+	// This kinds of quota must be requested on organization level.
+	QuotaKindTotalDeploymentsPerTierTemplate = "resourcemanager.total-deployment-%s"
 )
