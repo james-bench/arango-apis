a152fcc7d096b9e02079cf86871f2c2fbccf50ee
Ewout Prangsma <ewout@prangsma.net>
2019-10-12 09:24:28 +0200

    Added foreach helpers for resourcemanager api

--- common/v1/helpers.go
+++ common/v1/helpers.go
@@ -29,3 +29,20 @@ func CloneDuration(s *types.Duration) *types.Duration {
 	clone := *s
 	return &clone
 }
+
+// CloneOrDefault creates a clone of the given options if not nil,
+// or creates an empty ListOptions.
+// In either case, if current pageSize is zero, it is set to the given
+// default page size.
+func (opts *ListOptions) CloneOrDefault(defaultPageSize int32) *ListOptions {
+	if opts == nil {
+		opts = &ListOptions{}
+	} else {
+		clone := *opts
+		opts = &clone
+	}
+	if opts.PageSize == 0 {
+		opts.PageSize = defaultPageSize
+	}
+	return opts
+}
