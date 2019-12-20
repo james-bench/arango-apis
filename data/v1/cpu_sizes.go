f821baca8cabd503bd66624061eda25262c8dfb6
Ewout Prangsma <ewout@prangsma.net>
2019-12-20 10:07:41 +0100

    Added CPU size info to node sizes

---
+++ data/v1/cpu_sizes.go
@@ -0,0 +1,19 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package v1
+
+const (
+	// CPU sizes
+
+	// CPUSizeStandard is the CPU size that is used by default.
+	CPUSizeStandard = "standard"
+	// CPUSizeHigh is the CPU size that yields (on average) twice the CPU (per memory)
+	// compared to standard.
+	CPUSizeHigh = "high"
+)
