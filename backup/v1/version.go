783f57efddc7d08946fc36c3766956fe9131dd6a
Ewout Prangsma <ewout@prangsma.net>
2020-01-14 10:16:23 +0100

    Added version constants

---
+++ backup/v1/version.go
@@ -0,0 +1,18 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package v1
+
+const (
+	// APIMajorVersion contains major version of this API
+	APIMajorVersion = 1
+	// APIMinorVersion contains minor version of this API
+	APIMinorVersion = 0
+	// APIPatchVersion contains patch version of this API
+	APIPatchVersion = 0
+)
