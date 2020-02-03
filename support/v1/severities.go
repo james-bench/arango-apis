e5a4abbfaf02ee411e8d3783abd3ca0466d73928
Gergely Brautigam <gergely@arangodb.com>
2020-02-03 13:41:01 +0100

    Added severity

---
+++ support/v1/severities.go
@@ -0,0 +1,22 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Author Gergely Brautigam
+//
+
+package v1
+
+const (
+	// Critical severity
+	SupportTicketCriticalSeverity = "Critical"
+	// High severity
+	SupportTicketHighSeverity = "High"
+	// Medium severity
+	SupportTicketMediumSeverity = "Medium"
+	// Low severity
+	SupportTicketLowSeverity = "Low"
+	// Lowest severity
+	SupportTicketLowestSeverity = "Lowest"
+)
