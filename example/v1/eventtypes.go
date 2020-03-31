1b04b9ab1f9970caeeeb57d6ea7d1c85540bce7c
Gergely Brautigam <gergely@arangodb.com>
2020-03-31 14:17:53 +0200

    Added missing event types

---
+++ example/v1/eventtypes.go
@@ -0,0 +1,34 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+// Author Gergely Brautigam
+//
+
+package v1
+
+const (
+	// Example installations event types
+
+	// EventTypeInstallationCreated is the type of event fired after an installations has been created
+	// SubjectID contains the ID of the installation.
+	EventTypeInstallationCreated = "example.exampledatasetinstallation.created"
+	// EventTypeInstallationDeleted is the type of event fired after an installations has been deleted
+	// SubjectID contains the ID of the installation.
+	EventTypeInstallationDeleted = "example.exampledatasetinstallation.deleted"
+)
