54e97268bd34adf5f5e7804be3b63d133866f4bb
Gergely Brautigam <gergely@arangodb.com>
2020-04-07 12:13:58 +0200

    Updated according to comment

--- example/v1/statuses.go
+++ example/v1/statuses.go
@@ -11,12 +11,12 @@ package v1
 const (
 	// Installation states
 
-	// InProgress means that the pod was created successfull and the data is being generated / imported
-	InProgress = "InProgress"
-	// Created means that the data has been successfully generated.
-	Created = "Created"
-	// Ready means that the data is ready to be used.
-	Ready = "Ready"
-	// Failed denotes a dataset which has failed to be created.
-	Failed = "Failed"
+	// ExampleInstallationStatusInProgress means that the pod was created successfull and the data is being generated / imported
+	ExampleInstallationStatusInProgress = "InProgress"
+	// ExampleInstallationStatusCreated means that the database has been successfully created.
+	ExampleInstallationStatusCreated = "Created"
+	// ExampleInstallationStatusReady means that the data is ready to be used.
+	ExampleInstallationStatusReady = "Ready"
+	// ExampleInstallationStatusFailed denotes a dataset which has failed to be created.
+	ExampleInstallationStatusFailed = "Failed"
 )
