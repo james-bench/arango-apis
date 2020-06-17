74b86b171b81659380d427cb2d3ac5007d9d69eb
Gergely Brautigam <gergely@arangodb.com>
2020-06-17 13:16:25 +0200

    OAS-1164 | Added formatting options for deployment logs.

---
+++ monitoring/v1/constants.go
@@ -0,0 +1,19 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Author Gergely Brautigam
+//
+
+package v1
+
+const (
+	// Formatting options
+
+	// FormatText defines the log format to be text
+	FormatText = "text"
+	// FormatJson defines the log format to be json.
+	// In JSON format, log lines are encoded as a single line JSON object.
+	FormatJSON = "json"
+)
