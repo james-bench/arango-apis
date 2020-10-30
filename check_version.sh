4ea2c400c43304e28fcd0b65d7596905c9c2e093
Gergely Brautigam <gergely@arangodb.com>
2020-10-30 17:27:45 +0100

    Added disclaimer and ignore option

--- check_version.sh
+++ check_version.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+#
+# DISCLAIMER
+#
+# Copyright 2020 ArangoDB GmbH, Cologne, Germany
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright holder is ArangoDB GmbH, Cologne, Germany
+#
+# Author Gergely Brautigam
+#
+
 # Exit script if you try to use an uninitialized variable.
 set -o nounset
 
@@ -9,7 +31,7 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-ignore_string="ignore-ci:"
+ignore_string="ignore-version-check:"
 
 check_file() {
     fullpath=$1
