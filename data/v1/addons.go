9d0b884f40b23f38280bdfcaee4b1b085563b369
Robert Stam <git@stamware.com>
2021-04-19 16:51:24 +0200

    OAS-2571 | Added deployment addon IDs

---
+++ data/v1/addons.go
@@ -0,0 +1,21 @@
+//
+// DISCLAIMER
+//
+// Copyright 2021 ArangoDB GmbH, Cologne, Germany
+//
+// Author Robert Stam
+//
+
+package v1
+
+const (
+	// Addon IDs
+
+	// AddonIDIAMProvider is ID of the IAM provider addon.
+	// That addon is used when a deployment is using a custom IAM provider.
+	AddonIDIAMProvider = "iamprovider"
+
+	// AddonIDAuditLog is ID of the Audit Log addon.
+	// That addon is used when a deployment has an attached audit log.
+	AddonIDAuditLog = "auditlog"
+)
