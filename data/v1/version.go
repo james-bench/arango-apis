e3883f365a959d279e3abfa345c0326438fae39f
Robert Stam <git@stamware.com>
2022-01-20 11:15:37 +0100

    Updated version

--- data/v1/version.go
+++ data/v1/version.go
@@ -1,7 +1,7 @@
 //
 // DISCLAIMER
 //
-// Copyright 2020-2021 ArangoDB GmbH, Cologne, Germany
+// Copyright 2020-2022 ArangoDB GmbH, Cologne, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ const (
 	// APIMajorVersion contains major version of this API
 	APIMajorVersion = 2
 	// APIMinorVersion contains minor version of this API
-	APIMinorVersion = 4
+	APIMinorVersion = 5
 	// APIPatchVersion contains patch version of this API
-	APIPatchVersion = 9
+	APIPatchVersion = 0
 )
