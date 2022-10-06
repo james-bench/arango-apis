7ae53e9edb6b9d35642af36eb4c75a6172fc9889
Mayank Shah <mayank.shah@arangodb.com>
2022-10-06 12:27:56 +0530

    Add message struct
    Signed-off-by: Mayank Shah <mayank.shah@arangodb.com>

---
+++ replication/v1/message.go
@@ -0,0 +1,27 @@
+//
+// DISCLAIMER
+//
+// Copyright 2022 ArangoDB GmbH, Cologne, Germany
+//
+
+package v1
+
+// Message holds any additional information that the user must know regarding
+// the replication / migration process.
+type Message struct {
+	// Reason for any failures / conditions.
+	Reason string `json:"reason,omitempty"`
+	// Errors related to database that may occur during replication.
+	DbErrors string `json:"db_errors,omitempty"`
+	// Errors related to ArangoSync that may occur during replication.
+	SyncErrors string `json:"sync_errors,omitempty"`
+}
+
+// NewMessage returns a new Message type with the given reason, dbErrors and syncErrors strings.
+func NewMessage(reason, dbErrors, syncErrors string) Message {
+	return Message{
+		Reason:     reason,
+		DbErrors:   dbErrors,
+		SyncErrors: syncErrors,
+	}
+}
