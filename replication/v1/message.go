e9bcc36de609d2c440e7c7ac7e2eff84625310c3
Mayank Shah <mayank.shah@arangodb.com>
2022-10-19 13:13:24 +0530

    Add AsJSON helper
    Signed-off-by: Mayank Shah <mayank.shah@arangodb.com>

--- replication/v1/message.go
+++ replication/v1/message.go
@@ -6,6 +6,8 @@
 
 package v1
 
+import "encoding/json"
+
 // Message holds any additional information that the user must know regarding
 // the replication / migration process.
 type Message struct {
@@ -15,6 +17,12 @@ type Message struct {
 	DbErrors string `json:"db_errors,omitempty"`
 	// Errors related to ArangoSync that may occur during replication.
 	SyncErrors string `json:"sync_errors,omitempty"`
+}
+
+// AsJSON returns a JSON representation of the `Message` struct.
+func (m Message) AsJSON() string {
+	b, _ := json.Marshal(m)
+	return string(b)
 }
 
 // NewMessage returns a new Message type with the given reason, dbErrors and syncErrors strings.
