a94ad233ddd68a57b230a6afa5c64e09e16e2dd8
Adonis Murati <adonis.murati@arangodb.com>
2022-11-04 13:56:45 +0100

    OAS-5953 | Add phase updated at property to Deployment Replication

--- typescript/replication/v1/replication.ts
+++ typescript/replication/v1/replication.ts
@@ -99,6 +99,11 @@ export interface DeploymentReplication_Status {
   // This is a readonly field.
   // string
   sync_endpoint?: string;
+  
+  // Information when a particular phase was updated.
+  // This is a readonly field.
+  // googleTypes.Timestamp
+  phase_updated_at?: googleTypes.Timestamp;
 }
 
 // ReplicationService is the API used to replicate a deployment.
