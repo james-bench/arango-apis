9c10e8f4242e5e3026885b285634b060e31c8c2e
Adonis Murati <adonis.murati@arangodb.com>
2022-11-23 09:00:31 +0100

    OAS-5981 | add the deployment profile field to datad

--- data/v1/version.go
+++ data/v1/version.go
@@ -28,5 +28,5 @@ const (
 	// APIMinorVersion contains minor version of this API
 	APIMinorVersion = 6
 	// APIPatchVersion contains patch version of this API
-	APIPatchVersion = 1
+	APIPatchVersion = 2
 )
