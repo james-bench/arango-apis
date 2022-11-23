632126923a07c5bd45bd635d594486e232bbcec9
Adonis Murati <adonis.murati@arangodb.com>
2022-11-23 22:58:19 +0100

    OAS-5981 | generate

--- deployment-profile/v1/doc.go
+++ deployment-profile/v1/doc.go
@@ -21,4 +21,4 @@
 // Package deployment-profile/v1 contains the API of Deployment Profile service.
 package v1
 
-//go:generate protoc -I .:../../:../../vendor/:../../vendor/googleapis/:../../vendor/github.com/gogo/protobuf/protobuf/ --gofast_out=Mgithub.com/golang/protobuf/ptypes/duration/duration.proto=github.com/gogo/protobuf/types,Mgithub.com/golang/protobuf/ptypes/timestamp/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:. --grpc-gateway_out=paths=source_relative,logtostderr=true,allow_delete_body=true:. ./deployment-profile.proto
+//go:generate protoc -I .:../../:../../vendor/:../../vendor/googleapis/:../../vendor/github.com/gogo/protobuf/protobuf/ --gofast_out=Mgithub.com/golang/protobuf/ptypes/duration/duration.proto=github.com/gogo/protobuf/types,Mgithub.com/golang/protobuf/ptypes/timestamp/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:. --grpc-gateway_out=paths=source_relative,logtostderr=true,allow_delete_body=true:. ./deploymentprofile.proto
--- typescript/deployment-profile/v1/deploymentprofile.ts
+++ typescript/deployment-profile/v1/deploymentprofile.ts
@@ -8,7 +8,7 @@ import { Empty as arangodb_cloud_common_v1_Empty } from '../../common/v1/common'
 import { ListOptions as arangodb_cloud_common_v1_ListOptions } from '../../common/v1/common'
 import { Version as arangodb_cloud_common_v1_Version } from '../../common/v1/common'
 
-// File: deployment-profile/v1/deployment-profile.proto
+// File: deployment-profile/v1/deploymentprofile.proto
 // Package: arangodb.cloud.deploymentprofile.v1
 
 // DeploymentProfile represents a single Deployment Profile for an organization.
