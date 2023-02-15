3929162530854ced44715face6179c086c0cc173
Lakhan Samani <lakhan.samani@arangodb.com>
2023-02-15 12:13:35 +0530

    OAS-6639 | add resource type

---
+++ auth/v1/constants.go
@@ -0,0 +1,29 @@
+//
+// DISCLAIMER
+//
+// Copyright 2022 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+
+package v1
+
+const (
+	// Resource Types are used to idenitfy which resource service should be used
+	// to validate the permission for a given resource URI
+
+	// ResourceTypeNotebook is used to identify notebook service
+	ResourceTypeNotebook = "notebook"
+)
