d6485981f6b59abde9dcbc686bcc22a6381051af
Mayank Shah <mayank.shah@arangodb.com>
2023-10-06 15:05:48 +0530

    add foreach
    Signed-off-by: Mayank Shah <mayank.shah@arangodb.com>

--- credits/v1/foreach.go
+++ credits/v1/foreach.go
@@ -1,6 +1,5 @@
 //
 // DISCLAIMER
-//
 // Copyright 2023 ArangoDB GmbH, Cologne, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,6 +24,8 @@ import "context"
 type (
 	// CreditUsageCallback is a callback for each credit usage item.
 	CreditUsageCallback func(context.Context, *CreditBundleUsage) error
+	// CreditUsageReportCallback is a callback for each credit usage report.
+	CreditUsageReportCallback func(context.Context, *CreditUsageReport) error
 )
 
 // ForEachCreditUsage iterates over all credit usages that match the given filter,
@@ -34,6 +35,35 @@ func ForEachCreditUsage(
 	listFunc func(ctx context.Context, req *ListCreditBundleUsageRequest) (*CreditBundleUsageList, error),
 	req *ListCreditBundleUsageRequest,
 	cb CreditUsageCallback,
+) error {
+	req.Options = req.Options.CloneOrDefault()
+	for {
+		list, err := listFunc(ctx, req)
+		if err != nil {
+			return err
+		}
+		for _, item := range list.GetItems() {
+			if err := cb(ctx, item); err != nil {
+				return err
+			}
+			if err := ctx.Err(); err != nil {
+				return err
+			}
+		}
+		if len(list.GetItems()) < int(req.GetOptions().GetPageSize()) {
+			return nil
+		}
+		req.Options.Page++
+	}
+}
+
+// ForEachCreditUsageReport iterates over all credit usage reports that match the given filter,
+// and invoke the callback for each item.
+func ForEachCreditUsageReport(
+	ctx context.Context,
+	listFunc func(ctx context.Context, req *ListCreditUsageReportsRequest) (*CreditUsageReportList, error),
+	req *ListCreditUsageReportsRequest,
+	cb CreditUsageReportCallback,
 ) error {
 	req.Options = req.Options.CloneOrDefault()
 	for {
